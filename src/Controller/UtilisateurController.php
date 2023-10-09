319079200d94e2f17420a9572da662bf4faf70db
dilhann <dilhan.nathan@gmail.com>
2023-10-09 12:12:06 +0200

    TD2 - Exercice 4 - Formulaire de connexion

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UtilisateurController extends AbstractController
 {
@@ -47,6 +48,14 @@ class UtilisateurController extends AbstractController
         return $this->render("/utilisateur/inscription.html.twig", [
 //            "utilisateurs" => $utilisateurRepository->findAllOrderedByDate(),
             "formulaire" => $form
+        ]);
+    }
+
+    #[Route('/connexion', name: 'connexion', methods:["GET", "POST"])]
+    public function connexion(AuthenticationUtils $authenticationUtils) : Response {
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('utilisateur/connexion.html.twig', [
+            "lastusername" => $lastUsername
         ]);
     }
 }
